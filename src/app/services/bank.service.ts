09bb9cd63f28d9df27e246899d8e8b8e7a882b22
christeena <christeenapaulson@gmail.com>
2021-02-17 12:29:05 +0530

    bankappang

--- src/app/services/bank.service.ts
+++ src/app/services/bank.service.ts
@@ -1,91 +1,91 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class BankService {
-  accountDetails:any = {
+    accountDetails: any = {
 
-    userone: { acno: 1000, name: "ajay", balance: 1000, username: "userone", password: "testuser",history:[] },
-    usertwo: { acno: 1001, name: "sajay", balance: 20000, username: "usertwo", password: "testuser1",history:[] },
-    userthree: { acno: 1002, name: "vijay", balance: 25000, username: "userthree", password: "testuser2",history:[] },
+        userone: { acno: 1000, name: "ajay", balance: 1000, username: "userone", password: "testuser", history: [] },
+        usertwo: { acno: 1001, name: "sajay", balance: 20000, username: "usertwo", password: "testuser1", history: [] },
+        userthree: { acno: 1002, name: "vijay", balance: 25000, username: "userthree", password: "testuser2", history: [] },
 
 
-}
-authenicateuser = (uname:string,pwd:string) => {
-  
-  let dataset = this.accountDetails;
+    }
+    authenicateuser = (uname: string, pwd: string) => {
 
-  if (uname in dataset) {
-      if (dataset[uname].password == pwd) {
-          alert("login successful")
-          //window.location.href = "user.html"
-      }
-      else {
-          alert("incorrect password")
-      }
-  }
-  else {
-      alert("no user exist with provided username")
-  }
-}
+        let dataset = this.accountDetails;
 
-  //constructor() { }
-  deposit = (uname:string,pwd:string,amt:any) => {
-    // let uname = document.querySelector("#uname").value
-    // let pwd = document.querySelector("#pwd").value
-    // let amt = Number(document.querySelector("#amt").value)
-    let dataset = this.accountDetails;
-    let user =this.authenicateuser(uname,pwd);
-    if (uname in dataset) {
-        if (dataset[uname].password == pwd) {
-
-            dataset[uname].balance +=parseInt( amt)
-            dataset[uname].history.push({
-                amount:amt,typeOfTransaction:'credit'
-            })
-            alert("your account credited with amount" + amt + "avail bal=" + dataset[uname].balance)
+        if (uname in dataset) {
+            if (dataset[uname].password == pwd) {
+                alert("login successful")
+                //window.location.href = "user.html"
+            }
+            else {
+                alert("incorrect password")
+            }
         }
         else {
-            alert("incorrect password")
+            alert("no user exist with provided username")
         }
     }
-    else {
-        alert("no user exist with provided username")
+
+    //constructor() { }
+    deposit = (uname: string, pwd: string, amt: any) => {
+        // let uname = document.querySelector("#uname").value
+        // let pwd = document.querySelector("#pwd").value
+        // let amt = Number(document.querySelector("#amt").value)
+        let dataset = this.accountDetails;
+        let user = this.authenicateuser(uname, pwd);
+        if (uname in dataset) {
+            if (dataset[uname].password == pwd) {
+
+                dataset[uname].balance += parseInt(amt)
+                dataset[uname].history.push({
+                    amount: amt, typeOfTransaction: 'credit'
+                })
+                alert("your account credited with amount" + amt + "avail bal=" + dataset[uname].balance)
+            }
+            else {
+                alert("incorrect password")
+            }
+        }
+        else {
+            alert("no user exist with provided username")
+        }
     }
-}
-withdraw = (uname:string,pwd:string,amt:number) => {
-  let user = this.authenicateuser(uname,pwd);
-  // let uname = document.querySelector("#uname").value
-  // let pwd = document.querySelector("#pwd").value
-  // let amt = Number(document.querySelector("#amt").value)
-  let dataset = this.accountDetails
-  if (uname in dataset) {
-      if (dataset[uname].password == pwd) {
-          if (dataset[uname].balance >= amt) {
+    withdraw = (uname: string, pwd: string, amt: number) => {
+        let user = this.authenicateuser(uname, pwd);
+        // let uname = document.querySelector("#uname").value
+        // let pwd = document.querySelector("#pwd").value
+        // let amt = Number(document.querySelector("#amt").value)
+        let dataset = this.accountDetails
+        if (uname in dataset) {
+            if (dataset[uname].password == pwd) {
+                if (dataset[uname].balance >= amt) {
 
 
 
-              dataset[uname].balance -= amt
-              dataset[uname].history.push({
-                  amount:amt, typeOfTransaction:'debit'
-              })
-              alert("your account debited with amount" + amt + "avail bal=" + dataset[uname].balance)
+                    dataset[uname].balance -= amt
+                    dataset[uname].history.push({
+                        amount: amt, typeOfTransaction: 'debit'
+                    })
+                    alert("your account debited with amount" + amt + "avail bal=" + dataset[uname].balance)
 
-          }
-          else {
-              alert("insufficient balance")
-          }
-      }
-      else {
-          alert("no user exist with provided username")
-      }
-  }
+                }
+                else {
+                    alert("insufficient balance")
+                }
+            }
+            else {
+                alert("no user exist with provided username")
+            }
+        }
 
-}
-gethistory(){
-    let dataset=this.accountDetails
-    return dataset["userone"].history;
-}
+    }
+    gethistory() {
+        let dataset = this.accountDetails
+        return dataset["userone"].history;
+    }
 
 }
